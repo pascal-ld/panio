d6764709d04cd7d8f5d8d629c7415a541bd2fa60
pascal <pascal@lino-design.com>
2026-06-03 22:29:19 +0200

    Fix read only jwt volume

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,4 +5,8 @@ mkdir -p /var/www/html/public/uploads/products
 chown -R www-data:www-data /var/www/html/public/uploads
 chmod -R 775 /var/www/html/public/uploads
 
+mkdir -p /var/www/html/config/jwt
+chown -R www-data:www-data /var/www/html/config/jwt
+chmod 755 /var/www/html/config/jwt
+
 exec "$@"
